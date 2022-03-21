f6912fc9542915bbca4b6b9523266e66e45693f0
CIKADA <cikada@gmail.com>
2022-03-21 23:16:25 +0300

    init project

---
+++ resources/js/router/router.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/layout/Home";
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+];
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(process.env.BASE_URL),
+});
+
+export default router;
